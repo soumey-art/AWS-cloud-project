9603c35ed75e899bb74e50e7be01fe7f2025aa69
rith <sovanrithronn@gmail.com>
2026-07-20 11:51:28 +0700

    fix: consolidate output files (remove duplicate outputs.tf)

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,1 +1,34 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
 
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS name for accessing the application"
+  value       = aws_lb.application_lb.dns_name
+}
+
+output "rds_endpoint" {
+  description = "RDS instance endpoint"
+  value       = aws_db_instance.main.endpoint
+}
+
+output "s3_bucket_name" {
+  description = "S3 bucket name for file storage"
+  value       = aws_s3_bucket.main.bucket
+}
+
+output "asg_name" {
+  description = "Auto Scaling Group name"
+  value       = aws_autoscaling_group.app_asg.name
+}
